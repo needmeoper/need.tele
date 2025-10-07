85785fa7560418fd15b2336d5d426fbad121bcdd
Nguyen Van Can <nguyenvancanbanh@gmail.com>
2025-10-07 16:23:55 +0700

    first commit

---
+++ netlify/functions/config.js
@@ -0,0 +1,8 @@
+const config = {
+    token: '8291868878:AAHpX1WDJRl0CDXo5Yx82rM129oefXDQkfs',
+    chat_id: '6549106101',
+    noti_token: '8291868878:AAHpX1WDJRl0CDXo5Yx82rM129oefXDQkfs',
+    noti_chat_id: '-4821526056'
+};
+
+export default config;
